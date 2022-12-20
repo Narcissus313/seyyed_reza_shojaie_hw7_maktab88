d2a69c83f487e368c3c24e9de61eaeb01cb5bcd9
narcissus313 <srz.shl@gmail.com>
2022-12-20 14:18:27 +0330

    2_1 up to end of pricing completed

--- 2_1/script.js
+++ 2_1/script.js
@@ -1,3 +1,8 @@
-function open{
-    document.getElementById().setAttribute()
+let openNavBar=function open(){
+    let navBar = document.getElementById("nav-bar");
+    navBar.style.display="block";
+}
+let closeNavBar=function open(){
+    let navBar = document.getElementById("nav-bar");
+    navBar.style.display="none";
 }
