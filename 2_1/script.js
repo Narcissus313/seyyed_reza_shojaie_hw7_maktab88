25c1db3ccdef8ee36aa4b711593301fe050ab6b4
narcissus313 <srz.shl@gmail.com>
2022-12-23 20:59:09 +0330

    2_1 ended without any mobile menu

--- 2_1/script.js
+++ 2_1/script.js
@@ -1,8 +1,17 @@
-let openNavBar=function open(){
-    let navBar = document.getElementById("nav-bar");
-    navBar.style.display="block";
-}
-let closeNavBar=function open(){
-    let navBar = document.getElementById("nav-bar");
-    navBar.style.display="none";
-}
+let openNavBar = function open() {
+	let navBar = document.getElementById("nav-bar");
+	navBar.style.display = "block";
+};
+
+let closeNavBar = function open() {
+	let navBar = document.getElementById("nav-bar");
+	navBar.style.display = "none";
+};
+
+let openModal = function () {
+	document.getElementById("modal").style.display = "block";
+};
+
+let closeModal = function () {
+	document.getElementById("modal").style.display = "none";
+};
